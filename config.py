dd1b97a633cb8eab0fa10c9247bc1034278f2615
Alfredo Vania - IZ7BOJ <38103662+IZ7BOJ@users.noreply.github.com>
2022-04-10 18:23:54 +0200

    added txen and rxen pin declaration

--- config.py
+++ config.py
@@ -14,15 +14,15 @@ TCP_PORT = 10001
 ## Hardware settings
 # Begin LoRa radio and set NSS, reset, busy, IRQ, txen, and rxen pin with connected Raspberry Pi gpio pins
 # IRQ pin must be used, otherwise this software doesn't work (cannot be set to -1).
-# txen and rxen pin are not used by this software so are set to -1
+# txen and rxen pin must be used for correct management or the rx/tx switch of the LoRa module
 
 busId = 0 #SPI Bus ID. Must be enabled on raspberry (sudo raspi-config). Default is 0
 csId = 0 #SPI Chip Select pin. Valid values are 0 or 1
 resetPin = 22
 busyPin = 23
 irqPin = 26
-txenPin = -1 #txenPin = 5
-rxenPin = -1 #rxenPin = 25
+txenPin = 5
+rxenPin = 25
 
 ## LoRa Settings (only most used, the others are left by default)
 frequency = 433775000 #frequency in Hz
@@ -35,5 +35,5 @@ outputPower = 0 #maximum TX power is 22(dBm)
 TX_OE_Style = True #if True, tx RF packets are in OE Style, otherwise in standard AX25
 sync_word = 0x1424 #sync word is x4y4. Es: 0x12 of 1st gen LoRa chip --> 0x1424 of 2nd gen LoRa chip
 payloadLength = 50 #max payload length
-crcType = False #crc check
+crcType = True #crc check
 RX_GAIN_POWER_SAVING = False #If false, receiver is set in boosted gain mode (consume more power)
